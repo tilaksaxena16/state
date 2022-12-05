9ce1386d58c38475ce6a50b56d054e86c9d62230
Your Name <you@example.com>
2022-12-05 15:13:45 +0530

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { isLabelWithInternallyDisabledControl } from "@testing-library/user-event/dist/utils";
 import React, { useState } from "react";
 import "./App.css";
 
@@ -10,14 +11,36 @@ const App = () => {
     setCtime(newTime);
   };
 
+  const [myStyle, setMyStyle] = useState({
+    color: 'black',
+    backgroundcolor: 'white'
+  })
+
+  const toggle = () => {
+    if (myStyle.backgroundColor === 'white',myStyle.color === 'black') {
+      setMyStyle({
+        color: 'white',
+        backgroundColor: 'black'
+      })
+    }else{
+      setMyStyle({
+        color: 'black',
+        backgroundColor: 'white'
+      })
+    }
+  }
+
   return (
     <>
-      <div className="container">
+      <div className="container" style={myStyle}>
         <h1> {ctime} </h1>
-        <button className="button" onClick={UpdateTime}>Get Time</button>
+        <button type="button" onClick={UpdateTime} className="btn btn-primary">Get Time</button>
+        <div className="form-check form-switch toggle" onClick={toggle}>
+          <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+          <label className="form-check-label">Enable Dark Mode</label>
+        </div>
       </div>
     </>
-
   )
 }
 
